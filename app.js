c8f466b7291cb28f2369e0c6e547c19f9a6c13ec
BladimirDelBarrioPizarro <bladimaltego@gmail.com>
2018-09-29 16:41:33 +0200

    Added body-parser and nodemon

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 'use strict'
 
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
+const port = process.env.PORT || 3000
 
-app.listen(3000,() => {
-	console.log('API REST patient')
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
+app.listen(port, () => {
+	console.log(`API REST patient in http://localhost:${port}`)
 })
